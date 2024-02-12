f52a2c0a4a1badfc6a6f1d571f371cf6e730f766
Himanshi Sharma <himanshish456@gmail.com>
2024-02-12 20:48:57 +0530

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import classes from "./navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import person from "../../../public/person.png";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleShowDropdown = () => setShowDropdown((prev) => true);
+  const handleHideDropdown = () => setShowDropdown((prev) => false);
+
+  const loggedIn = false;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2 className={classes.left}>
+          <Link href="/">Blogs-App</Link>
+        </h2>
+        <ul className={classes.right}>
+          {loggedIn ? (
+            <div>
+              <Image
+                onClick={handleShowDropdown}
+                src={person}
+                width="45"
+                height="45"
+                alt=""
+              />
+              {showDropdown && (
+                <div className={classes.dropdown}>
+                  <AiOutlineClose
+                    className={classes.closeIcon}
+                    onClick={handleHideDropdown}
+                  />
+                  <button
+                    onClick={handleHideDropdown}
+                    className={classes.logout}
+                  >
+                    Logout
+                  </button>
+                  <Link
+                    onClick={handleHideDropdown}
+                    href="/create-post"
+                    className={classes.create}
+                  >
+                    Create
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <>
+              <button className={classes.login}>Log in</button>
+              <Link href="/register">Register</Link>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
