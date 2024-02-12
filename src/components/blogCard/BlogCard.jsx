307a1b0806e2bcdc2241a198f53a0065e5227fdb
Himanshi Sharma <himanshish456@gmail.com>
2024-02-12 21:30:42 +0530

    home-page ui

--- src/components/blogCard/BlogCard.jsx
+++ src/components/blogCard/BlogCard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import classes from "./blogCard.module.css";
-const BlogCard = () => {
-  return <div>Blog Card</div>;
+import Image from "next/image";
+import Link from "next/link";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
+const BlogCard = ({ blog: { title, desc, img } }) => {
+  const isLiked = true;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <Link className={classes.imgContainer} href="/">
+          <Image src={img} width="350" height="350" />
+        </Link>
+        <div className={classes.blogData}>
+          <div className={classes.left}>
+            <h3>{title}</h3>
+            <p>{desc}</p>
+            <span>
+              Created By: <span>1st of January</span>
+            </span>
+          </div>
+          <div className={classes.right}>
+            {12}{" "}
+            {isLiked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogCard;
