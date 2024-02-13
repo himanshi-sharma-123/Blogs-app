87cfe3277fe7a3d64e8a980246cba716a88cb99f
Himanshi Sharma <himanshish456@gmail.com>
2024-02-13 21:02:15 +0530

    blog get put update and delete

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -2,3 +2,41 @@
 
 //files inside must be named as route.js
 // http://localhost:3000/api/blog/someId
+
+import db from "@/lib/db";
+import { verifyToken } from "@lib/jwt";
+import Blog from "@/models/Blog";
+import { verifyJwtToken } from "@/lib/jwt";
+
+export async function GET(req) {
+  await db.connect();
+  try {
+    const blogs = await Blog.find({}).limit(16).populate("authorId");
+    return new Response(JSON.stringify(blogs), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
+
+export async function POST(req) {
+  await db.connect();
+
+  //[Bearer toeknjfjdfjhf]
+  const accessToken = req.headers.get("authorization"); //in nodejs -> req.headers.authorization
+  const token = accessToken.split(" ")[1];
+
+  const decodedToken = verifyJwtToken(token);
+  if (!accessToken || !decodedToken) {
+    return new Response(
+      JSON.stringify({ error: "unauthorized (wrong or expired token)" }),
+      { status: 403 }
+    );
+  }
+  try {
+    const body = await req.json();
+    const newBlog = await Blog.create(body);
+    return new Response(JSON.stringify(newBlog), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify(null), { status: 500 });
+  }
+}
